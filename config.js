dceb8c49b83564d12695ea3679899e4e38452a05
nconrad <nealconrad@gmail.com>
2014-11-05 16:16:08 -0600

    adding app service url to config

--- config.js
+++ config.js
@@ -4,9 +4,10 @@ angular.module('appTasker').service('config', function() {
 
     this.ui = {name: "App Tasker"};
 
-    this.services = {ws_url: "https://kbase.us/services/ws",
+    this.services = {app_url: "http://140.221.66.219:7124",
+                     ws_url: "https://kbase.us/services/ws",
                      fba_url: "https://kbase.us/services/KBaseFBAModeling",
                      auth_url: "https://kbase.us/services/authorization/Sessions/Login",
-                 	 shock_url: "http://140.221.67.190:7078"};
+                     shock_url: "http://140.221.67.190:7078"};
 
 })
