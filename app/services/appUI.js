cbcdcffde9d80db16d273d46c2ace14e84774380
nconrad <nealconrad@gmail.com>
2014-11-20 22:30:58 -0600

    turning on polling

--- app/services/appUI.js
+++ app/services/appUI.js
@@ -16,9 +16,9 @@ angular.module('appTasker')
     var self = this;
 
     // how often to update tasks/status (in ms)
-    var polling = false;
+    var polling = true;
     var pollTasksMS = 5000;
-    var pollStatusMS = 4000;    
+    var pollStatusMS = 3000;    
     var taskDispCount = 50; 
 
     // default workspace; used at the start of the application
@@ -76,6 +76,8 @@ angular.module('appTasker')
     this.updateStatus = function() {
         $http.rpc('app', 'query_task_summary')
             .then(function(res) {
+
+                console.log(res)
                 self.status = {queued: 'queued' in res ? res.queued : 0,
                                inprogress: 'in-progress' in res ? res['in-progress'] : 0,
                                completed: 'completed' in res ? res.completed : 0};
@@ -185,77 +187,6 @@ angular.module('appTasker')
         }
         return cols;
     }
-
-
-    /*
-    var auth = {token: authService.token}
-    var appService = new AppService(config.services.app_url, auth)
-
-    appService.enumerate_tasks(0, 25).done(function(data){
-        console.log('app', data)
-    }).fail(function(e){
-        console.log('failed', e)
-    })*/    
-
-    // initial fetch of user's writable workspace list
-    /*
-    this.getWS = $http.rpc('ws', 'list_workspace_info', {perm: 'w'} )
-        .then(function(workspaces) {
-        var workspaces = workspaces.sort(compare)
-
-        function compare(a,b) {
-            var t1 = uiTools.getTimestamp(b[3]) 
-            var t2 = uiTools.getTimestamp(a[3]) 
-            if (t1 < t2) return -1;
-            if (t1 > t2) return 1;
-            return 0;
-        }
-
-        var ws_list = [];
-        for (var i in workspaces) {
-            ws_list.push({name: workspaces[i][1], 
-                          id: workspaces[i][0], 
-                          count: workspaces[i][4]});
-        }
-
-        self.ws_list = ws_list;
-    });
-
-
-    // initial fetch of ws object list
-    this.getObjs = $http.rpc('ws', 'list_objects', {workspaces: [self.current_ws] } )
-    .then(function(data){
-        self.ws_objects = data;
-
-        var types = {};
-        for (var i in data) {
-            var type = data[i][2].split('-')[0];
-            var obj = {name: data[i][1], id: data[i][0]};
-
-            if (type in types) {
-                types[type].push(obj);
-            } else {
-                types[type] = [obj];
-            }
-        }
-
-        self.wsObjsByType = types;
-    }).catch(function(e){
-        console.log('here', e)
-    });
-
-    
-    // method to update ws object list, called on change
-    this.updateWSObjs = function(new_ws) {
-        var p = $http.rpc('ws', 'list_objects', {workspaces: [new_ws]})
-        .then(function(ws_objects) {
-            self.ws_objects = ws_objects;
-        }, function(e) {
-            console.log('fail', e)
-        })        
-        return p;
-    }*/
-
 
 }]);
 
